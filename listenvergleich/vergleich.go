ac237208879726b7641f86db17dd906b4e6fee68
Reiner Hüchting <dev@reinerhuechting.de>
2023-02-12 00:36:55 +0100

    Lösung für den Vergleich zweier Listen hinzugefügt.

--- listenvergleich/vergleich.go
+++ listenvergleich/vergleich.go
@@ -2,6 +2,11 @@ package listenvergleich
 
 // Liefert true, falls die beiden Listen gleich sind.
 func ListsEqual(list1, list2 []int) bool {
-	// TODO
-	return false
+	if len(list1) != len(list2) {
+		return false
+	}
+	if len(list1) == 0 {
+		return true
+	}
+	return list1[0] == list2[0] && ListsEqual(list1[1:], list2[1:])
 }
