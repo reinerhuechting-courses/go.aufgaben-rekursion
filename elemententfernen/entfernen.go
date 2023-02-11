1dde735fb2081b03934bd53dd41d581d33aba38f
Reiner Hüchting <dev@reinerhuechting.de>
2023-02-12 00:30:07 +0100

    Lösung für RemoveElement hinzugefügt.

--- elemententfernen/entfernen.go
+++ elemententfernen/entfernen.go
@@ -3,6 +3,11 @@ package elemententfernen
 // Liefert eine Liste, die alle Elemente aus list enthält,
 // außer dem an Stelle pos.
 func RemoveElement(list []int, pos int) []int {
-	// TODO
-	return nil
+	if pos < 0 || len(list) == 0 {
+		return list
+	}
+	if pos == 0 {
+		return list[1:]
+	}
+	return append(list[0:1], RemoveElement(list[1:], pos-1)...)
 }
