8756f09f1a10eb5ded000df1ca9d09ed51fb2dae
Reiner Hüchting <dev@reinerhuechting.de>
2023-02-12 00:34:51 +0100

    Lösungen für die Filterfunktionen hinzugefügt.

--- filter/filter.go
+++ filter/filter.go
@@ -2,12 +2,24 @@ package filter
 
 // Liefert eine Liste mit allen Elementen aus list, die kleiner oder gleich key sind.
 func FilterLess(list []int, key int) []int {
-	// TODO
-	return nil
+	if len(list) == 0 {
+		return []int{}
+	}
+	head, tail := list[0], FilterLess(list[1:], key)
+	if head <= key {
+		return append([]int{head}, tail...)
+	}
+	return tail
 }
 
 // Liefert eine Liste mit allen Elementen aus list, die größer als key sind.
 func FilterGreater(list []int, key int) []int {
-	// TODO
-	return nil
+	if len(list) == 0 {
+		return []int{}
+	}
+	head, tail := list[0], FilterGreater(list[1:], key)
+	if head > key {
+		return append([]int{head}, tail...)
+	}
+	return tail
 }
